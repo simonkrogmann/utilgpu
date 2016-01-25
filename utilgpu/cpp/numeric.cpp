e2ad564ba8a5f5ac237bc2b908e621dd4c96fcd6
Simon Krogmann <simon.krogmann@student.hpi.de>
2016-01-25 02:37:25 +0100

    Fix frame time display bugs

--- utilgpu/cpp/numeric.cpp
+++ utilgpu/cpp/numeric.cpp
@@ -7,11 +7,11 @@ namespace util
 std::vector<int> toDigits(unsigned int number)
 {
     std::vector<int> digits;
-    while (number > 0)
+    do
     {
         digits.push_back(number % 10);
         number /= 10;
-    }
+    } while (number > 0);
     std::reverse(digits.begin(), digits.end());
     return digits;
 }
